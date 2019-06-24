3a0d58ed906d5f351b7eae261bfc5b6ea750459c
Hosung Lim <holnet1026@gmail.com>
2019-06-24 22:08:11 +0900

    refactor: optimize code

--- src/containers/UnsplashContainer/UnsplashContainer.jsx
+++ src/containers/UnsplashContainer/UnsplashContainer.jsx
@@ -1,41 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import ThumbnailList from 'components/unsplash/ThumbnailList';
 import * as UnsplashAPI from 'lib/api/unsplash';
 import SearchForm from "components/unsplash/SearchForm/SearchForm";
 import ScrollContainer from "components/base/ScrollContainer";
 
+const convertData = (record) => ({
+  id: record.id,
+  title: record.alt_description,
+  url: record.urls.thumb,
+  author: record.user.username
+ });
+
 const UnsplashContainer = () => {
-  console.log('test');
   const [images, setImages] = useState([]);
   const [selected, setSelected] = useState(null);
 
-  const fetchRandomImage = async () => {
+  const fetchRandomImage = useCallback(async () => {
     const { data } = await UnsplashAPI.getRandomPhotos({ count: 30 });
-    setImages(data.map((record) => ({
-      id: record.id,
-      title: record.alt_description,
-      url: record.urls.thumb,
-      author: record.user.username
-    })));
-  };
+    setImages(data.map(convertData));
+  },[]);
 
   const searchImage = async (query) => {
     if(!query) {
       await fetchRandomImage();
       return;
     }
-    const { data } = await UnsplashAPI.searchPhotos({ query });
-    setImages(data.results.map((record) => ({
-      id: record.id,
-      title: record.alt_description,
-      url: record.urls.thumb,
-      author: record.user.username
-    })));
+     const { data } = await UnsplashAPI.searchPhotos({ query });
+     setImages(data.results.map(convertData));
   };
 
   useEffect(  () => {
     fetchRandomImage();
-  }, []);
+  }, [fetchRandomImage]);
 
 
   return (
