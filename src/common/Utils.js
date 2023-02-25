9c6b061ad33624cee68658e097b5dbcbad9d8456
godori <gej.okpo@gmail.com>
2023-02-26 00:23:25 +0900

    fix - Add the missing code

--- src/common/Utils.js
+++ src/common/Utils.js
@@ -16,6 +16,7 @@ export const getRandomImageDataUrl = (width, height) =>
   fetch(
     `https://picsum.photos/seed/${getRandomNumber(1084)}/${width}/${height}`,
   )
+    .then((response) => response.blob())
     .then(
       (blob) =>
         new Promise((resolve) => {
